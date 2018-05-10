85b1d1188443b901b9b56028ffe57bd7f7c50918
Avi Avni <avi.avni@gmail.com>
2018-05-10 21:30:47 +0300

    implement azure cognitive services demo

---
+++ DeepLearningDemo.CognitiveServices/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Azure.CognitiveServices.Search.ImageSearch;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeepLearningDemo.CognitiveServices
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                var computerVisionApiKey = Environment.GetEnvironmentVariable("ComputerVisionApiKey");
+                using (var fs = new FileStream(ofd.FileName, FileMode.Open))
+                using (var api = new ComputerVisionAPI(new Microsoft.Azure.CognitiveServices.Vision.ComputerVision.ApiKeyServiceClientCredentials(computerVisionApiKey)))
+                {
+                    api.AzureRegion = AzureRegions.Westcentralus;
+                    result.DataContext = await api.AnalyzeImageInStreamAsync(fs, new List<VisualFeatureTypes> { VisualFeatureTypes.Adult, VisualFeatureTypes.Categories, VisualFeatureTypes.Color, VisualFeatureTypes.Description, VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType, VisualFeatureTypes.Tags });
+                }
+                imgVision.Source = new BitmapImage(new Uri(ofd.FileName));
+            }
+        }
+
+        private async void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            var imageSearchApiKey = Environment.GetEnvironmentVariable("ImageSearchApiKey");
+            using (var api = new ImageSearchAPI(new Microsoft.Azure.CognitiveServices.Search.ImageSearch.ApiKeyServiceClientCredentials(imageSearchApiKey)))
+            {
+                var res = await api.Images.SearchAsync(search.Text);
+                imagesSearch.ItemsSource = res.Value;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DeepLearningDemo
 Using F#(and C#) to demo CNTK(and Azure Cognitive Services)  for http://www.seladeveloperpractice.com/
+
+# Steps to run
+1. Obtain Azure Cognitive Services Computer Vision and Image Search Api keys from https://azure.microsoft.com/en-us/services/cognitive-services/?&ls=Search&lsd=SEM&WT.srch=1&WT.mc_id=AID529458_SEM_wSgSQpj6
+2. Add environment variables "ComputerVisionApiKey" and "ImageSearchApiKey" with the correspond value from step 1
+3. Open the DeepLearningDemo.sln
+4. Run the DeepLearningDemo.CognitiveServices
+5. In the first tab browse and select image to be analyed
+6. In the second tab write something and hit search button to find images
